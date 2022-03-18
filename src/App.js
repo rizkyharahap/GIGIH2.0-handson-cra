94b2f2492784e800f4444616b3c6774319720101
rizkyharahap <rizkyhh1999@gmail.com>
2022-03-18 19:06:38 +0700

    Add test for env file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // Test for api key
+  console.log("API KEY : ", process.env.REACT_APP_API_KEY);
+
   return (
     <div className="App">
       <header className="App-header">
